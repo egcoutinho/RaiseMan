f6174d24974bc36d0ed39b688dc90148ee4af270
Eduardo Coutinho <e.g.coutinho@gmail.com>
2019-05-14 07:57:08 -0300

    Initial Commit

---
+++ RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by Eduardo G. Coutinho on 14/05/19.
+//  Copyright © 2019 CyberShark. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
