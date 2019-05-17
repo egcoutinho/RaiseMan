f03ba3b864fc0d05a6be53342d58817bae0b2cda
Eduardo Coutinho <e.g.coutinho@gmail.com>
2019-05-16 23:52:15 -0300

    Primeira versão

---
+++ RaiseMan/Employee.swift
@@ -0,0 +1,15 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Eduardo G. Coutinho on 14/05/19.
+//  Copyright © 2019 CyberShark. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject
+{
+    @objc var name: String? = "Novo funcionário"
+    @objc var raise: Float = 0.05
+}
