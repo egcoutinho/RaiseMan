5b159c0e4d9ce70815b4e74411b053edc9b6406f
Eduardo Coutinho <e.g.coutinho@gmail.com>
2019-05-18 16:34:01 -0300

    Inclui tratamento de exceção quando campo "Aumento" é deixado em branco

--- RaiseMan/Employee.swift
+++ RaiseMan/Employee.swift
@@ -12,4 +12,20 @@ class Employee: NSObject
 {
     @objc var name: String? = "Novo funcionário"
     @objc var raise: Float = 0.05
+
+    override func validateValue(_ ioValue: AutoreleasingUnsafeMutablePointer<AnyObject?>, forKey inKey: String) throws
+    {
+        let raiseNumber = ioValue.pointee
+        if inKey == "raise" && raiseNumber == nil
+        {
+            let domain = "UserInputValidationErrorDomain"
+            let code = 0
+            let userInfo = [NSLocalizedDescriptionKey : "An employee's raise must be a number."]
+            throw NSError(domain: domain, code: code, userInfo: userInfo )
+
+//            print("default raise = 0.0")
+//            raise = 0.0
+            
+        }
+    }
 }
