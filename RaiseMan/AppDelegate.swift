8ef107b5cf3c9d31c853d9f7b3414bf125dcdc59
Eduardo Coutinho <e.g.coutinho@gmail.com>
2019-05-18 13:28:02 -0300

    .

--- RaiseMan/AppDelegate.swift
+++ RaiseMan/AppDelegate.swift
@@ -11,9 +11,6 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate
 {
-
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification)
     {
         // Insert code here to initialize your application
